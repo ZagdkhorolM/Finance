0524df88cd1a8e5d10fa544408b86ffa195e917e
Zagdaa13 <zagaa113@gmail.com>
2023-07-05 19:34:09 +0800

    Event

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
-var uiController = (function() {
-    var x = 100;
-  
-    function add(y) {
-      return x + y;
+var uiController = (function () {})();
+
+var financeController = (function () {})();
+
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдөл өө олно
+    console.log("Дүн оруулсан.");
+    // 2. Олж авсан өгөгдөлүүдээ санхүүгийн контроллерт хадгална.
+    // 3. Олж авсан өгөгдөлүүдээ веб дээрээ тохирох хэсэгт гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.witch === 13) {
+      ctrlAddItem();
     }
-  
-    return {
-      publicAdd: function(a) {
-        a = add(a);
-        console.log("Боловсруулсан утга : " + a);
-      }
-    };
-  })();
-  
-  var financeController = (function() {})();
-  
-  var appController = (function(uiController, financeController) {
-    uiController.publicAdd(150);
-  })(uiController, financeController);
-  
-  // var hunController = (function() {
-  
-  //     // data encapsulation
-  
-  //     // private data
-  //   var bodol = "Javascript толгой эргүүлмээр юм...";
-  //   function tsusGuih() {}
-  //     // private function
-  //   function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
-  
-  //   return {
-  //     yarih: function() {
-  //       bodol = "Javascript бол лаг";
-  //       huchilturugchiigAgaaraasSorjTsusruuOruulah();
-  //       tsusGuih();
-  //       console.log("hi");
-  //     }
-  //   };
-  // })();
+  });
+})(uiController, financeController);
